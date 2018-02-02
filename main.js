5023214753e8728887fc39e4f44039d5975c6ca1
katsisaac50 <33658908+katsisaac50@users.noreply.github.com>
2018-02-02 15:34:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('btn');
+
+function reqListener(event) {
+    //console.log(this.responseText);
+
+    var xmlHTTPRequest = new XMLHttpRequest();
+    xmlHTTPRequest.open("GET", "https://developer.github.com/v3/", true);
+    xmlHTTPRequest.onload = function(event) {
+        console.log('done');
+        console.log(event);
+        console.log(xmlHTTPRequest.responseText);
+    };
+    xmlHTTPRequest.send();
+}
+button.addEventListener("click", reqListener);
